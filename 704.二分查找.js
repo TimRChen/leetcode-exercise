d2ceb9d75864b944065f5b2ed4378ba21571c44d
TimRChen <527410313@qq.com>
2019-05-10 18:56:27 +0800

    704.二分查找.js

---
+++ 704.二分查找.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = left + (right - left >> 1);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
+
